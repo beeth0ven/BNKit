2f1a925026d4a59017eee707de649c7f28bd7f36
beeth0ven <beeth0vendev@163.com>
2016-11-17 10:00:29 +0800

    Add public

--- BNKit/Source/UIKit+/UIColor+Flat.swift
+++ BNKit/Source/UIKit+/UIColor+Flat.swift
@@ -11,195 +11,195 @@ import UIKit
 
 extension UIColor {
     
-    class var flatPowerBlueLight: UIColor {
+    public class var flatPowerBlueLight: UIColor {
         return UIColor(hex: "#ABBAF2")!
     }
     
-    class var flatPowerBlueDark: UIColor {
+    public class var flatPowerBlueDark: UIColor {
         return UIColor(hex: "#8898D0")!
     }
     
-    class var flatSkyBlueLight: UIColor {
+    public class var flatSkyBlueLight: UIColor {
         return UIColor(hex: "#3182D9")!
     }
     
-    class var flatSkyBlueDark: UIColor {
+    public class var flatSkyBlueDark: UIColor {
         return UIColor(hex: "#2769B0")!
     }
     
-    class var flatBlueLight: UIColor {
+    public class var flatBlueLight: UIColor {
         return UIColor(hex: "#404E95")!
     }
     
-    class var flatBlueDark: UIColor {
+    public class var flatBlueDark: UIColor {
         return UIColor(hex: "#2C3872")!
     }
     
-    class var flatPurpleLight: UIColor {
+    public class var flatPurpleLight: UIColor {
         return UIColor(hex: "#5F42C0")!
     }
     
-    class var flatPurpleDark: UIColor {
+    public class var flatPurpleDark: UIColor {
         return UIColor(hex: "#472F97")!
     }
     
-    class var flatMagentaLight: UIColor {
+    public class var flatMagentaLight: UIColor {
         return UIColor(hex: "#853DAE")!
     }
     
-    class var flatMagentaDark: UIColor {
+    public class var flatMagentaDark: UIColor {
         return UIColor(hex: "#7728A4")!
     }
     
-    class var flatPinkLight: UIColor {
+    public class var flatPinkLight: UIColor {
         return UIColor(hex: "#E960BB")!
     }
     
-    class var flatPinkDark: UIColor {
+    public class var flatPinkDark: UIColor {
         return UIColor(hex: "#C34291")!
     }
     
-    class var flatWatermolonLight: UIColor {
+    public class var flatWatermolonLight: UIColor {
         return UIColor(hex: "#E35A66")!
     }
     
-    class var flatWatermolonDark: UIColor {
+    public class var flatWatermolonDark: UIColor {
         return UIColor(hex: "#C93F45")!
     }
     
-    class var flatRedLight: UIColor {
+    public class var flatRedLight: UIColor {
         return UIColor(hex: "#D93829")!
     }
     
-    class var flatRedDark: UIColor {
+    public class var flatRedDark: UIColor {
         return UIColor(hex: "#AC281C")!
     }
     
-    class var flatOrangeLight: UIColor {
+    public class var flatOrangeLight: UIColor {
         return UIColor(hex: "#D96C00")!
     }
     
-    class var flatOrangeDark: UIColor {
+    public class var flatOrangeDark: UIColor {
         return UIColor(hex: "#C24100")!
     }
     
-    class var flatYellowLight: UIColor {
+    public class var flatYellowLight: UIColor {
         return UIColor(hex: "#FBC700")!
     }
     
-    class var flatYellowDark: UIColor {
+    public class var flatYellowDark: UIColor {
         return UIColor(hex: "#F99A00")!
     }
     
-    class var flatLimeLight: UIColor {
+    public class var flatLimeLight: UIColor {
         return UIColor(hex: "#98BF00")!
     }
     
-    class var flatLimeDark: UIColor {
+    public class var flatLimeDark: UIColor {
         return UIColor(hex: "#7FA500")!
     }
     
-    class var flatGreenLight: UIColor {
+    public class var flatGreenLight: UIColor {
         return UIColor(hex: "#3BC651")!
     }
     
-    class var flatGreenDark: UIColor {
+    public class var flatGreenDark: UIColor {
         return UIColor(hex: "#31A343")!
     }
     
-    class var flatMintLight: UIColor {
+    public class var flatMintLight: UIColor {
         return UIColor(hex: "#2EB187")!
     }
     
-    class var flatMintDark: UIColor {
+    public class var flatMintDark: UIColor {
         return UIColor(hex: "#27916F")!
     }
     
-    class var flatTealLight: UIColor {
+    public class var flatTealLight: UIColor {
         return UIColor(hex: "#305B70")!
     }
     
-    class var flatTealDark: UIColor {
+    public class var flatTealDark: UIColor {
         return UIColor(hex: "#2C4F60")!
     }
     
-    class var flatNavyBlueLight: UIColor {
+    public class var flatNavyBlueLight: UIColor {
         return UIColor(hex: "#28384D")!
     }
     
-    class var flatNavyBlueDark: UIColor {
+    public class var flatNavyBlueDark: UIColor {
         return UIColor(hex: "#222E40")!
     }
     
-    class var flatBlackLight: UIColor {
+    public class var flatBlackLight: UIColor {
         return UIColor(hex: "#202020")!
     }
     
-    class var flatBlackDark: UIColor {
+    public class var flatBlackDark: UIColor {
         return UIColor(hex: "#1D1D1D")!
     }
     
-    class var flatBrownLight: UIColor {
+    public class var flatBrownLight: UIColor {
         return UIColor(hex: "#4A3625")!
     }
     
-    class var flatBrownDark: UIColor {
+    public class var flatBrownDark: UIColor {
         return UIColor(hex: "#3E2D1F")!
     }
     
-    class var flatCoffeeLight: UIColor {
+    public class var flatCoffeeLight: UIColor {
         return UIColor(hex: "#90745B")!
     }
     
-    class var flatCoffeeDark: UIColor {
+    public class var flatCoffeeDark: UIColor {
         return UIColor(hex: "#7A5F49")!
     }
     
-    class var flatSandLight: UIColor {
+    public class var flatSandLight: UIColor {
         return UIColor(hex: "#EBD89F")!
     }
     
-    class var flatSandDark: UIColor {
+    public class var flatSandDark: UIColor {
         return UIColor(hex: "#CAB77D")!
     }
     
-    class var flatWhiteLight: UIColor {
+    public class var flatWhiteLight: UIColor {
         return UIColor(hex: "#E8ECEE")!
     }
     
-    class var flatWhiteDark: UIColor {
+    public class var flatWhiteDark: UIColor {
         return UIColor(hex: "#B0B6BB")!
     }
     
-    class var flatGrayLight: UIColor {
+    public class var flatGrayLight: UIColor {
         return UIColor(hex: "#849495")!
     }
     
-    class var flatGrayDark: UIColor {
+    public class var flatGrayDark: UIColor {
         return UIColor(hex: "#6D797A")!
     }
     
-    class var flatMaroonLight: UIColor {
+    public class var flatMaroonLight: UIColor {
         return UIColor(hex: "#62231E")!
     }
     
-    class var flatMaroonDark: UIColor {
+    public class var flatMaroonDark: UIColor {
         return UIColor(hex: "#501C18")!
     }
     
-    class var flatForestGreenLight: UIColor {
+    public class var flatForestGreenLight: UIColor {
         return UIColor(hex: "#2B4E2F")!
     }
     
-    class var flatForestGreenDark: UIColor {
+    public class var flatForestGreenDark: UIColor {
         return UIColor(hex: "#254026")!
     }
     
-    class var flatPlumLight: UIColor {
+    public class var flatPlumLight: UIColor {
         return UIColor(hex: "#49244E")!
     }
     
-    class var flatPlumDark: UIColor {
+    public class var flatPlumDark: UIColor {
         return UIColor(hex: "#3C1D40")!
     }
     
