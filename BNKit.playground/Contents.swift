5a035fafc45dbdc4414c74fd6052542375292be7
beeth0ven <beeth0vendev@163.com>
2016-12-12 00:18:59 +0800

    1

--- BNKit.playground/Contents.swift
+++ BNKit.playground/Contents.swift
@@ -7,3 +7,6 @@ import RxSwift
 import RxCocoa
 import UIKit
 
+// Filter -> SearchResult -> [Section] -> CellStyle -> Cell
+// Filter -async-> SearchResult -sync-> [Section] -sync-> CellStyle -sync-> Cell
+// Filter -> Observable<SearchResult> -> [Section] -> CellStyle -> Cell
