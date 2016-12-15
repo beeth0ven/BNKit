6c154f66d6031897bb91a98a47ce532e327a1382
beeth0ven <beeth0vendev@163.com>
2016-12-15 08:59:58 +0800

    1

--- BNKit.playground/Contents.swift
+++ BNKit.playground/Contents.swift
@@ -2,12 +2,13 @@
  # BNKit
  Use this playground to try out BNKit
  */
+import PlaygroundSupport
 import BNKit
 import RxSwift
 import RxCocoa
 import UIKit
 
-playgroundShouldContinueIndefinitely()
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 Observable<TimeInterval>.timer(duration: 5, interval: 1)
     .debug("timer")
